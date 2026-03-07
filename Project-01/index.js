6750c07d134f580838216adf3b330137a5a2273a
Abhishek Pandey <abhishekpandey.codes@gmail.com>
2026-03-07 07:51:08 +0530

    initial commit

---
+++ Project-01/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express();
+const PORT = 8000;
+
+//Defining routes and path of our express app
+
+
+
+
+
+
+
+
+
+
+
+//Setting up the server
+app.listen(PORT, ()=>console.log(`Server started at ${PORT}`));
